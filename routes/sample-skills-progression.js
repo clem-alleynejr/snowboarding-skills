3cdfc980613d55f4172ddb39fda15379e3ee3be0
clem-alleynejr <clem.alleynejr@gmail.com>
2024-01-19 00:06:39 -0700

    sample list of skills added

--- routes/sample-skills-progression.js
+++ routes/sample-skills-progression.js
@@ -1,12 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
-router.get('/sample-skills-progression', function (req, res, next) {
-    res.render('sample-skills-progression/index', {
-      viewType: 'Sample Skills/Progression',
-      user: req.user
-    });
+router.get("/", function (req, res, next) {
+  res.render("sample-skills-progression/index", {
+    title: "Sample Skills/Progression",
+    viewType: "Sample Skills/Progression",
+    user: req.user,
   });
+});
 
-  module.exports = router;
+module.exports = router;
