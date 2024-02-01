b6be6386265ffe6ef7eddb764e4585c44b4be019
clem-alleynejr <clem.alleynejr@gmail.com>
2024-01-31 18:07:48 -0700

    added UIfor editing and deleting comments and added routers for edit and update

--- routes/snowboarding-skills.js
+++ routes/snowboarding-skills.js
@@ -3,15 +3,20 @@ const router = express.Router();
 // You'll be creating this controller module next
 const snowboardingSkillsCtrl = require('../controllers/snowboarding-skills');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
+const snowboardingSkills = require('../controllers/snowboarding-skills');
 	
 // GET /snowboarding-skills
-router.get('/', snowboardingSkillsCtrl.index);
+router.get('/', ensureLoggedIn, snowboardingSkillsCtrl.index);
 // GET /snowboarding-skills/new
 router.get('/new', ensureLoggedIn, snowboardingSkillsCtrl.new);
 // POST /snowboarding-skills
 router.post('/', ensureLoggedIn, snowboardingSkillsCtrl.create);
 // POST /snowboarding-skills/:id/notes-comments
 router.post('/:id/notes-comments', ensureLoggedIn, snowboardingSkillsCtrl.createNoteComment)
+// show note/comment edit page for a specific note/comment
+router.get('/:snowboardingSkillId/notes-comments/:noteCommentId/edit', ensureLoggedIn, snowboardingSkillsCtrl.editNoteComment)
+// update the specific note/comment
+router.put('/:snowboardingSkillId/notes-comments/:noteCommentId', ensureLoggedIn, snowboardingSkillsCtrl.updateNoteComment)
 // GET /snowboarding-skills/:id (show functionality) MUST be below new route
 router.get('/:id', ensureLoggedIn, snowboardingSkillsCtrl.show);
 // DELETE /snowboarding-skills/:id 
