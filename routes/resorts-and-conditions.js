bd1fe839e5073c39a57cd222b4507fc218091450
clem-alleynejr <clem.alleynejr@gmail.com>
2024-04-24 00:38:24 -0600

    added the fetch code for getting all resorts

--- routes/resorts-and-conditions.js
+++ routes/resorts-and-conditions.js
@@ -3,6 +3,24 @@ var router = express.Router();
 const passport = require("passport");
 
 router.get("/", function (req, res, next) {
+  
+  // const resort = req.query.resort;
+  
+  const url = 'https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '4e1e820731msh235cf838225aa6dp1dc113jsn8dd04336584a',
+      'X-RapidAPI-Host': 'ski-resorts-and-conditions.p.rapidapi.com'
+    }
+  };
+  
+  fetch(url, options)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+
   res.render("resorts-and-conditions/index", {
     title: 'Resorts and Conditions',
     viewType: 'Resorts and Conditions',
